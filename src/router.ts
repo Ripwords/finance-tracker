3512a7c638a72bebe8faf48611d5573ba26ada1a
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2022-02-17 18:43:02 +0800

    change to webhash

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from '@ionic/vue-router'
+import { createWebHashHistory, createRouter } from '@ionic/vue-router'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { RouteRecordRaw } from 'vue-router'
 
@@ -51,7 +51,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
